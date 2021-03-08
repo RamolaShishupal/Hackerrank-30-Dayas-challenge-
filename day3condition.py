aacb1fc8c54d77d58cd3e3e70b5802e398adaf4f
Your Name <you@example.com>
2021-03-08 15:03:41 +0530

    5 add

---
+++ day3condition.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import sys
+
+weird = False
+N = int(input().strip())
+if (N % 2 == 1) or (N % 2 == 0 and N >= 6 and N <= 20):
+    weird = True
+    
+if weird:
+    print("Weird")
+else:
+    print("Not Weird")
